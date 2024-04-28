73f52ae3a2368dc9c6bb83eba378cd5e9a8b92c0
Guilherme Andrade <96486731+guilhermeandradee@users.noreply.github.com>
2024-04-28 12:56:30 -0300

    v2

--- src/components/ProductsMain.jsx
+++ src/components/ProductsMain.jsx
@@ -1,4 +1,8 @@
 import './productsMain.css';
+import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowBack } from "react-icons/io";
+
+
 
 const ProductsMain = () => {
     return(
@@ -7,9 +11,31 @@ const ProductsMain = () => {
                 <h1>Populares</h1>
             </div>
             <main className='productsmain-main'>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div className='arrow-main'>
+                    <IoIosArrowBack className='arrow-main '/>
+                </div>
+
+                <div className='item-product-main-container'>
+                    <div className='item-product-main'>
+                        <div className='item-product-main-img-container' style={{
+                            backgroundImage: 'url(https://images.pexels.com/photos/19987317/pexels-photo-19987317/free-photo-of-arte-padronizar-padrao-estrutura.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)'
+                        }}>
+                            
+                        </div>
+                    </div>
+
+                    <div className='item-product-main'>
+                        a
+                    </div>
+
+                    <div className='item-product-main'>
+                        a
+                    </div>
+                </div>
+
+                <div >
+                    <IoIosArrowForward className='arrow-main '/>
+                </div>
             </main>
         </div>
     )
