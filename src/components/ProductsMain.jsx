be5da5f53374c3e9d52d247c18edcd0edcd66c4b
Guilherme Andrade <96486731+guilhermeandradee@users.noreply.github.com>
2024-04-30 18:28:54 -0300

    add construction

--- src/components/ProductsMain.jsx
+++ src/components/ProductsMain.jsx
@@ -1,6 +1,8 @@
 import './productsMain.css';
 import { IoIosArrowForward } from "react-icons/io";
 import { IoIosArrowBack } from "react-icons/io";
+import { FaArrowDown } from "react-icons/fa";
+
 
 
 
@@ -16,21 +18,82 @@ const ProductsMain = () => {
                 </div>
 
                 <div className='item-product-main-container'>
+
                     <div className='item-product-main'>
-                        <div className='item-product-main-img-container' style={{
-                            backgroundImage: 'url(https://images.pexels.com/photos/19987317/pexels-photo-19987317/free-photo-of-arte-padronizar-padrao-estrutura.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)'
-                        }}>
+                        
+                        <div className='item-product-main-img-container' >
+
+                            <div className='item-product-main-img' style={{
+                                backgroundImage: 'url(https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?auto=compress&cs=tinysrgb&w=600)'
+                            }}>    
+                            </div>
                             
+
+                        </div>
+
+                        <div className='item-product-main-description'>
+                            <div className='price-main-container'>
+                                <h1>R$ 58,99</h1>
+                                <div className='price-main-promotion'>
+                                    <FaArrowDown/> 16%
+                                </div>
+                            </div>
+                            <p>Nike Dunk mummy branco limited edition plus plas bing</p>
                         </div>
                     </div>
 
-                    <div className='item-product-main'>
-                        a
-                    </div>
+                    
 
                     <div className='item-product-main'>
-                        a
+                        
+                        <div className='item-product-main-img-container' >
+                            <div className='item-product-main-img' style={{
+                            backgroundImage: 'url(https://images.pexels.com/photos/12628400/pexels-photo-12628400.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)'
+                            }}>    
+                            </div>
+
+                            
+
+                        </div>
+
+                        <div className='item-product-main-description'>
+                            <div className='price-main-container'>
+                                <h1>R$ 58,99</h1>
+                                <div className='price-main-promotion'>
+                                    <FaArrowDown/> 16%
+                                </div>
+                            </div>
+                            <p>Nike Dunk mummy branco limited edition</p>
+                        </div>
                     </div>
+
+
+                    
+
+
+                    <div className='item-product-main'>
+                        
+                        <div className='item-product-main-img-container' >
+
+                            <div className='item-product-main-img' style={{
+                            backgroundImage: 'url(https://images.pexels.com/photos/3261069/pexels-photo-3261069.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)'
+                            }}>    
+                            </div>
+                            
+
+                        </div>
+
+                        <div className='item-product-main-description'>
+                            <div className='price-main-container'>
+                                <h1>R$ 58,99</h1>
+                                <div className='price-main-promotion'>
+                                    <FaArrowDown /> 16%
+                                </div>
+                            </div>
+                            <p>Nike Dunk mummy branco limited edition</p>
+                        </div>
+                    </div>
+
                 </div>
 
                 <div >
